f9b7f6f5a9eb1439f142a74aa4400c02802db947
Colby Aley <colby@aley.me>
2013-12-18 08:39:18 -0800

    Use grid with nice images, add individual image page, and start planning out admin side of things

--- app.rb
+++ app.rb
@@ -2,7 +2,12 @@ require 'sinatra'
 require 'sinatra/flash'
 require File.expand_path('../environment.rb', __FILE__)
 Dir[File.dirname(__FILE__) + '/models/*.rb'].each {|model| require model }
-enable :sessions
+
+
+# Use secret from env variable, and expire sessions after a year.
+use Rack::Session::Cookie, expire_after: 31556926,
+                           key: 'lincolnstore',
+                           secret: ENV['SESSION_SECRET']
 
 get '/' do
   @products = Product.all.limit(9)
@@ -12,7 +17,21 @@ end
 # Admin Page
 get '/admin' do
   authenticate
+  @products = Product.all
+  @orders = []
   erb :'admin/index'
+end
+
+get '/admin/products/:id/edit' do
+  erb :'admin/products/edit'
+end
+
+get 'admin/products/new' do
+  erb :'admin/products/new'
+end
+
+post '/admin/products/new' do
+  redirect '/admin' if Product.create(params[:product])
 end
 
 # Products
